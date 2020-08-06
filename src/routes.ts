589d8541083535980723167140e48f41c695af41
Lucas Santos <lucasparaipaba113@gmail.com>
2020-08-06 18:36:23 -0300

    Routes of list and create connections completed

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import ClassController from './controllers/ClassController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 
 const classController = new ClassController();
+const connectionsController = new ConnectionsController();
+
 // GET: Busca ou atualiza uma informação
 // POST: Cria alguma informação
 // PUT: Atualiza uma informação ja existente
@@ -12,5 +15,7 @@ const classController = new ClassController();
 
 routes.get('/classes', classController.index);
 routes.post('/classes', classController.create);
+
+routes.post('/connections', connectionsController.create);
 
 export default routes;
